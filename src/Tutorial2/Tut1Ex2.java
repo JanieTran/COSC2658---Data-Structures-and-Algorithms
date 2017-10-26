ee645d62c19af187fa4feedf49aed76749408133
Janie <emerald1901@gmail.com>
2017-10-26 16:02:12 +0700

    Thu 26/10

---
+++ src/Tutorial2/Tut1Ex2.java
@@ -0,0 +1,21 @@
+package Tutorial2;
+
+public class Tut1Ex2 {
+    public static void main(String[] args) {
+        char c = 'a';
+        int i = (int) c;
+        System.out.println(i);
+
+        float f = 1000.34f;
+        i = (int) f;
+        System.out.println(i);
+
+        double d = 1000.34;
+        i = (int) d;
+        System.out.println(i);
+
+        i = 97;
+        c = (char) i;
+        System.out.println(c);
+    }
+}
